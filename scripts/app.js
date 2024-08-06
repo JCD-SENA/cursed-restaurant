cb3f530c9265ac31b43275feb06146308f0cbe0d
JCD <katuadaisnotreal@gmail.com>
2024-08-06 14:54:36 -0500

    Se implementa la base de parte 4

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,63 @@
 let catalogue = document.getElementById("main")
+let onCatalogue = []
 
 function loadCatalogue() {
 	products.forEach((product, i) => {
-		let container = document.createElement("article")
-		container.className = "producto"
-		let name = document.createElement("h2")
-		name.textContent = product["nombre"]
-		let image = document.createElement("img")
-		image.src = product["img"]
-		let button = document.createElement("button")
-		button.textContent = "Adquirir"
-		let price = document.createElement("h3")
-		price.textContent = product["precio"]+"₪"
-		container.value = product["valor real"]
-		container.id = i
-		container.appendChild(name)
-		container.appendChild(image)
-		container.appendChild(price)
-		container.appendChild(button)
-		catalogue.appendChild(container)
+		if (!onCatalogue.includes(i)) {
+			let container = document.createElement("article")
+			container.className = "producto"
+			let name = document.createElement("h2")
+			name.textContent = product["nombre"]
+			let image = document.createElement("img")
+			image.src = product["img"]
+			let button = document.createElement("button")
+			button.textContent = "Adquirir"
+			let price = document.createElement("h3")
+			price.textContent = "د.إ"+product["precio"]
+			container.value = product["valor real"]
+			container.id = i
+			container.appendChild(name)
+			container.appendChild(image)
+			container.appendChild(price)
+			container.appendChild(button)
+			catalogue.appendChild(container)
+			onCatalogue.push(i)
+		}
 	})
 }
 
-function checkIfAlreadyOnCatalogue () {
-	
-}
-
-function addCatalogue () {
-
+function addCatalogue (name, priceNumber, img) {
+	let artNumeral = ""
+	let price = priceNumber.toString()
+	for (let i = 0; i < price.length; i++) {
+		switch (price[i]) {
+			case "0": artNumeral += '٠';break;
+			case "1": artNumeral += '١';break;
+			case "2": artNumeral += '٢';break;
+			case "3": artNumeral += '٣';break;
+			case "4": artNumeral += '٤';break;
+			case "5": artNumeral += '٥';break;
+			case "6": artNumeral += '٦';break;
+			case "7": artNumeral += '٧';break;
+			case "8": artNumeral += '٨';break;
+			case "9": artNumeral += '٩';break;
+		}
+	}
+	console.log(artNumeral)
+	products.push({
+		"nombre": name,
+		"img": img,
+		"precio": artNumeral,
+		"valor real": price
+	})
+	loadCatalogue()
 }
 
 loadCatalogue();
+
+document.getElementById("footer").addEventListener("click", () => {
+	let themenoid = new Audio("https://vgmtreasurechest.com/soundtracks/club-penguin-original-soundtrack/eayrodluzi/29.%20Charlie%27s%20Here.mp3");
+    if (!playingMusic)
+        themenoid.play()
+    playingMusic = true;
+})
--- scripts/recursos.js
+++ scripts/recursos.js
@@ -1,46 +1,39 @@
 let playingMusic = false;
 let products = [
-    {
-        "nombre": "Le fishe au chocolat",
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyGI6tXAD0m95OqhLt5arPaDph327ir2ePsw&s",
-        "precio": "ר",
-        "valor real": 200
-    }, {
-        "nombre": "Delicia Inglesa",
-        "img": "https://i.imgflip.com/75el6y.jpg",
-        "precio": "שןה",
-        "valor real": 355
-    }, {
-        "nombre": "キッド東京",
-        "img": "https://qph.cf2.quoracdn.net/main-qimg-a455d6f1daa8e62350c0a35301609319-lq",
-        "precio": "ץיב",
-        "valor real": 912
-    }, {
-        "nombre": "Gatorade",
-        "img": "https://i.redd.it/yrmep4tk3rh91.jpg",
-        "precio": "ב׳ךד",
-        "valor real": 2024
-    }, {
-        "nombre": "Inicio del fin",
-        "img": "https://thehorrorzine.com/Morbid/DisgustingFoodMuseum/fruit%20bat%20soup.jpg",
-        "precio": "ב׳יט",
-        "valor real": 2020
-    }, {
-        "nombre": "Emberiza hortulana",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Emberiza_hortulana_2.jpg/226px-Emberiza_hortulana_2.jpg",
-        "precio": "םי״ו",
-        "valor real": 616
-    }, {
-        "nombre": "Headcrab",
-        "img": "https://static.wikia.nocookie.net/halflife7283/images/8/89/Heacrab_HL2_Model.jpg/revision/latest?cb=20131213222623&path-prefix=es",
-        "precio": "ג",
-        "valor real": 3
-    }
+	{
+		"nombre": "Le fishe au chocolat",
+		"img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyGI6tXAD0m95OqhLt5arPaDph327ir2ePsw&s",
+		"precio": "٢٠٠",
+		"valor real": 200
+	}, {
+		"nombre": "Delicia Inglesa",
+		"img": "https://i.imgflip.com/75el6y.jpg",
+		"precio": "٣٥٥",
+		"valor real": 355
+	}, {
+		"nombre": "キッド東京",
+		"img": "https://qph.cf2.quoracdn.net/main-qimg-a455d6f1daa8e62350c0a35301609319-lq",
+		"precio": "٩١٢",
+		"valor real": 912
+	}, {
+		"nombre": "Gatorade",
+		"img": "https://i.redd.it/yrmep4tk3rh91.jpg",
+		"precio": "٢٠٢٤",
+		"valor real": 2024
+	}, {
+		"nombre": "Inicio del fin",
+		"img": "https://thehorrorzine.com/Morbid/DisgustingFoodMuseum/fruit%20bat%20soup.jpg",
+		"precio": "٢٠٢٠",
+		"valor real": 2020
+	}, {
+		"nombre": "Emberiza hortulana",
+		"img": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Emberiza_hortulana_2.jpg/226px-Emberiza_hortulana_2.jpg",
+		"precio": "٦١٦",
+		"valor real": 616
+	}, {
+		"nombre": "Headcrab",
+		"img": "https://static.wikia.nocookie.net/halflife7283/images/8/89/Heacrab_HL2_Model.jpg/revision/latest?cb=20131213222623&path-prefix=es",
+		"precio": "٣",
+		"valor real": 3
+	}
 ]
-
-function musica () {
-    let themenoid = new Audio("https://vgmtreasurechest.com/soundtracks/club-penguin-original-soundtrack/eayrodluzi/29.%20Charlie%27s%20Here.mp3");
-    if (!playingMusic)
-        themenoid.play()
-    playingMusic = true;
-}
